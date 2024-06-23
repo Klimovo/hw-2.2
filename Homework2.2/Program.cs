8b95cdcc18cf2a2b3d834ab991151eb5627ed133
olegf <olegf@DESKTOP-1V337T0>
2024-06-23 23:34:17 +0300

    Добавьте файлы проекта.

---
+++ Homework2.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+
+        Console.WriteLine("Введіть рядок для фільтрації:");
+        string input = Console.ReadLine();
+
+        string[] forbiddenWords = { "bad", "word", "example" };
+
+        string pattern = @"\b(" + string.Join("|", forbiddenWords.Select(Regex.Escape)) + @")\b";
+
+        string filteredString = Regex.Replace(input, pattern, match => new string('#', match.Length), RegexOptions.IgnoreCase);
+
+        Console.WriteLine("Результат фільтрації:");
+        Console.WriteLine(filteredString);
+    }
+}
